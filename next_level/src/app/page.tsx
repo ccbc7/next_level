14c8ca0306798ae7739f60d3d57a3e091d6c6e89
ccbc7 <flexibleelbow@gmail.com>
2024-09-29 23:06:54 +0900

    引数の型を指定する

--- next_level/src/app/page.tsx
+++ next_level/src/app/page.tsx
@@ -1,95 +1,20 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const bool: boolean = true;
+  const num: number = 1;
+  const str: string = "AAA";
+  const arr1: Array<number> = [0, 1, 2];
+  const arr2: numner[] = [0, 1, 2];
+  const tuple: [number, string] = [0, "A"];
+  // 返り値がないということ
+  const funcA = (): void => {
+    const test = "TEST";
+  };
+  const undefind1: indefind = undefined;
+  const obj: { id: number, name: string } = { id: 0, name: "AAA" };
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  return (
+    <>
+      <h1>こんにちは</h1>
+    </>
   );
 }
---
+++ next_level/src/app/practice/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+export const Practice1 = () => {
+  const onClickFee = (num : number) => {
+    const total = num * 1.1;
+    console.log(total);
+  };
+
+  const onClickPractice = () => {
+    return onClickFee(1000);
+  };
+
+  return (
+    <>
+      <div>こんばんは</div>
+      <button onClick={onClickPractice}>練習問題１を実行</button>
+    </>
+  );
+};
+
+export default Practice1;
