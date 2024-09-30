e0744beeab9dc847ebe29b8f516b56c517e4f387
ccbc7 <flexibleelbow@gmail.com>
2024-09-30 23:19:12 +0900

    axiosインポートしたい

--- next_level/src/app/page.tsx
+++ next_level/src/app/page.tsx
@@ -1,30 +1,16 @@
-import Practice1 from "./practice/page"
-import Practice2 from "./practice2/page"
-import Practice3 from "./Practice3/page";
+import "./globals.css";
+import axios from "axios";
 
 export default function Home() {
+  const onClickFetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos");
+  };
+
   return (
     <>
-      <Practice1 />
-      <Practice2 />
-      <Practice3 />
+      <div className="App">
+        <button onClick={onClickFetchData}>データ取得</button>
+      </div>
     </>
   );
 }
-
-
-
-
-
-// const bool: boolean = true;
-// const num: number = 1;
-// const str: string = "AAA";
-// const arr1: Array<number> = [0, 1, 2];
-// const arr2: numner[] = [0, 1, 2];
-// const tuple: [number, string] = [0, "A"];
-// // 返り値がないということ
-// const funcA = (): void => {
-//   const test = "TEST";
-// };
-// const undefind1: indefind = undefined;
-// const obj: { id: number, name: string } = { id: 0, name: "AAA" };
